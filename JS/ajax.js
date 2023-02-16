a355485e0082911fa7debf03b73cf180fdbba1c3
sirandresitog <sirandresitog@gmail.com>
2023-02-16 14:41:05 -0500

    change ajax.html

--- JS/ajax.js
+++ JS/ajax.js
@@ -10,7 +10,7 @@
       //console.log(xhr.responseText);
       //$xhr.innerHTML=xhr.responseText;
       let json = JSON.parse(xhr.responseText);
-     // console.log(json);
+      // console.log(json);
       json.forEach((el) => {
         const $li = document.createElement("li");
         $li.innerHTML = `${el.name} <br> ${el.email} <br> ${el.phone}`;
@@ -18,7 +18,7 @@
       });
       $xhr.appendChild($fragment);
     } else {
-     // console.log("error");
+      // console.log("error");
       let message = xhr.statusText || "Ocurrio un error al traer los datos";
       $xhr.innerHTML = `Error: ${xhr.status}: ${message}`;
       //console.log("Este Mensaje cargara de cualquier forma");
@@ -28,4 +28,34 @@
   //consumo de json local (datos)
   //xhr.open("GET", "assets/users.json");
   xhr.send();
+})();
+(() => {
+  const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+  //console.log($fetch);
+  fetch("https://jsonplaceholder.typicode.com/users")
+    // .then((res) => {
+    //   console.log(res);
+    //   return res.ok ? res.json() : Promise.reject(res);
+    // })
+    //simplificacion de codigo (abajo then)
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      //console.log(json);
+      // $fetch.innerHTML = json;
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name}<br>${el.email}<br>${el.address.city}<br>${el.company.catchPhrase}`;
+        $fragment.appendChild($li);
+      });
+      $fetch.appendChild($fragment);
+    })
+    .catch((err) => {
+      //console.log(err);
+      let message = err.statusText || "Ocurrio un error";
+      $fetch.innerHTML = `Error: ${err.status}: ${message}`;
+    })
+    .finally(() => {
+      //console.log("Esto se ejecutara independientemente de la promesa FETCH");
+    });
 })();
